ef72225f45d30a470ea93a1fd276bfa1665cd606
dlfinis <dln.finis@gmail.com>
2016-06-22 12:41:17 -0500

    Add init files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sigma.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sigma
+ */
+angular.module('sigma')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
